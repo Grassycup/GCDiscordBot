34acd58cbd4d36c323b2bbb5667328ed5f5e325e
Sung <grassycup@gmail.com>
2017-08-03 22:29:19 -0400

    Improved Bot Functionality
    - Added shellscript for running Kaldi prediction
    - Added !help, !ping, !join, !leave commands

--- discordbot.js
+++ discordbot.js
@@ -3,13 +3,20 @@
 /**
  * Module dependencies.
  */
+const app = require('./app');
+const debug = require('debug')('gcdiscordbot:server');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
+const dotenv = require('dotenv');
 
-var app = require('./app');
-var debug = require('debug')('gcdiscordbot:server');
-var http = require('http');
-var path = require('path');
+// discord
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var dotenv = require('dotenv');
+
+
 if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
   dotenv.config({path: '.env'});
 } else {
@@ -21,14 +28,14 @@ if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -43,7 +50,7 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -67,7 +74,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -89,119 +96,152 @@ function onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
 
-// discord
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
+var curVoiceChannel;
 client.on('message', message => {
-  var channel = message.channel;
-  if (message.content === 'ping') {
-    //message.reply('pong');
-    channel.sendMessage('pong');
+  const channel = message.channel;
+  var command;
+  var commandSize;
+
+  // ignore bot's messages and determine whether the message is a command
+  if(!message.author.bot && message.content.startsWith('!')) {
+    // break up user message
+    command = message.content.split(' ');
+    commandSize = command.length;
   }
 
-  if(message.content === 'join') {
-    if (!message.guild) {
-      return message.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
-    }
-    const voiceChannel = message.guild.channels.find("name", 'voicetest');
-    //console.log(voiceChannel.id);
-    if (!voiceChannel || voiceChannel.type !== 'voice') {
-      return message.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
-    }
-    voiceChannel.join()
-      .then(conn => {
-        console.log('joined channel');
-        message.reply('ready!');
-        // create our voice receiver
-        const receiver = conn.createReceiver();
+  // validate command and make sure command isn't empty
+  if(command && command.length > 0) {
+    switch (command[0].toLowerCase()) {
+      case '!ping':
+        channel.sendMessage('pong');
+        break;
+      case '!join':
+        if (!message.guild) {
+          message.reply('This feature is not available');
+          return;
+        }
 
-        conn.on('speaking', (user, speaking) => {
-          if (speaking) {
-            //channel.sendMessage(`I'm listening to ${user}`);
-            // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
-            const audioStream = receiver.createPCMStream(user);
-            // create an output stream so we can dump our data in a file
-            const outputStream = generateOutputFile(voiceChannel, user);
-            console.log(user);
-            // pipe our audio data into the file stream
-            audioStream.pipe(outputStream);
-            // outputStream.on('data', (chunk) => {
-            //   console.log(chunk);
-            // });
-            // when the stream ends (the user stopped talking) tell the user
-            audioStream.on('end', () => {
-              convertAudio(outputStream.path, channel, user);
+        // set the channel name if there are enough parameters
+        const channelName = commandSize > 1 ? command[1] : null;
+        if(!channelName) {
+          message.reply('Please specify the voice channel to join');
+          return;
+        }
+        const voiceChannel = message.guild.channels.find("name", channelName);
+        if (!voiceChannel || voiceChannel.type !== 'voice') {
+          return message.reply(`I couldn't find the voice channel ${channelName}.`);
+        }
+        voiceChannel.join()
+          .then(conn => {
+            // save the current voice channel
+            curVoiceChannel = voiceChannel;
+            // create our voice receiver
+            const receiver = conn.createReceiver();
 
-              //channel.sendMessage(`I'm no longer listening to ${user}`);
+            conn.on('speaking', (user, speaking) => {
+              if (speaking) {
+                // this creates a 8-bit signed PCM, stereo from PCM file
+                const audioStream = receiver.createPCMStream(user);
+                // create an output stream so we can dump our data in a file
+                const outputStream = generateOutputFile(voiceChannel, user);
+                // pipe our audio data into the file stream
+                audioStream.pipe(outputStream);
+                // when the stream ends (the user stopped talking) tell the user
+                audioStream.on('end', () => {
+                  convertAudio(outputStream.path, channel, user);
+                });
+              }
             });
-          }
-        });
-      })
-      .catch(console.log);
-  }
+          })
+          .catch(console.log);
 
+        break;
+      case '!leave':
+        if(curVoiceChannel)
+          curVoiceChannel.leave();
+        curVoiceChannel = null;
+        break;
+      case '!help':
+        channel.sendMessage(`
+!help - Get help
+!join VoiceChannelName - Join specified voice channel and transcribe speech
+!ping - Pong
+          `);
+        break;
+      default:
+        channel.sendMessage('Command not found. Please use !help to see the list of commands');
+    }
+  }
 });
 
-const fs = require('fs');
-// make a new stream for each time someone starts to talk
-function generateOutputFile(channel, member) {
+
+
+/**
+ * create a new writable stream everytime someone starts to talk, which will then be used to write to file
+ * @param channel the channel where the command was executed from
+ * @param user name of the user that spoke
+ */
+function generateOutputFile(channel, user) {
   // use IDs instead of username cause some people have stupid emojis in their name
-  const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
+  const fileName = `./recordings/${channel.id}-${user.id}-${Date.now()}.pcm`;
   return fs.createWriteStream(fileName);
 }
 
-const { exec } = require('child_process');
-// convert audio files to flac audio 16khz sample rate 16bit mono channel
-function convertAudio(input, channel, user) {
-  //exec('ffmpeg -f s32le -i ' + input + ' -sample_fmt s16 -ar 16k -ac 1 -c:a flac ' + input + '.flac', (err, stdout, stderr) => {
-  exec('ffmpeg -f s32le -i ' + input + ' -acodec pcm_s16le -ar 8000 -ac 1 ' + input + '.wav', (err, stdout, stderr) => {
+/**
+ * convert audio files to flac audio 8khz sample rate 16bit mono channel
+ * @param filePath absolute path to the file that should get converted
+ * @param channel the channel where the prediction will be displayed in
+ * @param user name of the user that spoke
+ */
+function convertAudio(filePath, channel, user) {
+  exec('ffmpeg -f s32le -i ' + filePath + ' -acodec pcm_s16le -ar 8000 -ac 1 ' + filePath + '.wav', (err, stdout, stderr) => {
     if (err) {
       // node couldn't execute the command
       return;
     }
-    outputText(input, channel, user);
-
-    // // the *entire* stdout and stderr (buffered)
-    // console.log(`stdout: ${stdout}`);
-    // console.log(`stderr: ${stderr}`);
+    outputText(filePath, channel, user);
   });
 }
 
-function outputText(p, channel, user) {
+/**
+ *
+ * @param filePath absolute path to the file that should get converted
+ * @param channel the channel where the prediction will be displayed in
+ * @param user name of the user that spoke
+ */
+function outputText(filePath, channel, user) {
   console.log(path);
-  var fileName = path.basename(p);
+  var fileName = path.basename(filePath);
 
   exec('kaldi/egs/aspire/s5/predict.sh ../../../../recordings/' + fileName + '.wav', (err, stdout, stderr) => {
     var myRe = new RegExp('\nutterance-id1.*\n');
     var result = myRe.exec(stderr);
+
+    // don't display message if prediction yielded no results
+    if(!result)
+      return;
+
     var msg = user + ' said ' + result;
     msg = msg.replace('utterance-id1', '');
+    // clean up audio files since they're not needed anymore
     fs.unlink('/mnt/c/Users/Cardinality/Documents/GitHub/GCDiscordBot/recordings/' + fileName, function() { console.log('pcm removed'); });
     fs.unlink('/mnt/c/Users/Cardinality/Documents/GitHub/GCDiscordBot/recordings/' + fileName + '.wav', function() { console.log('wav removed'); });
     channel.sendMessage(msg);
   });
 }
 
+// start up the bot
 client.login(process.env.DiscordToken);
-
-// var fileName = '341356270588133377-146052390313787392-1501654600327.pcm.wav';
-// exec('kaldi/egs/aspire/s5/predict.sh ../../../../recordings/341356270588133377-146052390313787392-1501729762261.pcm.wav', (err, stdout, stderr) => {
-//   console.log(stderr);
-//   var myRe = new RegExp('\nutterance-id1.*\n');
-//   var result = myRe.exec(stderr);
-//   console.log(result[0].trim());
-// });
---
+++ predict.sh
@@ -0,0 +1,33 @@
+cd $(dirname $(readlink -f $0))
+
+#!/bin/bash
+. ./cmd.sh
+. ./path.sh
+
+result=$(online2-wav-nnet3-latgen-faster \
+  --online=false \
+  --do-endpointing=false \
+  --frame-subsampling-factor=3 \
+  --config=exp/tdnn_7b_chain_online/conf/online.conf \
+  --max-active=7000 \
+  --beam=15.0 \
+  --lattice-beam=6.0 \
+  --acoustic-scale=1.0 \
+  --word-symbol-table=exp/tdnn_7b_chain_online/graph_pp/words.txt \
+  exp/tdnn_7b_chain_online/final.mdl \
+  exp/tdnn_7b_chain_online/graph_pp/HCLG.fst \
+  'ark:echo utterance-id1 utterance-id1|' \
+  'scp:echo utterance-id1 '$1'|' \
+  'ark:/dev/null')
+
+
+
+
+
+
+
+
+
+
+
+
